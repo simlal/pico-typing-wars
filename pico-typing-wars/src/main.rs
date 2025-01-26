1e390104be45c31914a008787df9fc173180cb3b
Simon Lalonde <simlalonde@hotmail.com>
2025-01-26 17:58:01 -0500

    Test second led wiring

--- pico-typing-wars/src/main.rs
+++ pico-typing-wars/src/main.rs
@@ -13,23 +13,26 @@ async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
     let mut led_onboard = Output::new(p.PIN_25, Level::Low);
     let mut led_player_1 = Output::new(p.PIN_5, Level::Low);
+    let mut led_player_2 = Output::new(p.PIN_8, Level::Low);
 
     loop {
-        info!("Turning onboard led pin output to high...");
+        //info!("Turning onboard led pin output to high...");
         led_onboard.set_high();
         info!("led on!");
         Timer::after_secs(1).await;
 
-        info!("Turning onboard led pin output to low...");
+        //info!("Turning onboard led pin output to low...");
         led_onboard.set_low();
         info!("led off!");
         Timer::after_secs(1).await;
 
         // player1 test
         led_player_1.set_high();
-        info!("player1 quick-on/off");
+        led_player_2.set_high();
+        info!("player1/2 quick-on/off");
         Timer::after_millis(500).await;
         led_player_1.set_low();
+        led_player_2.set_low();
         Timer::after_millis(500).await;
     }
 }
